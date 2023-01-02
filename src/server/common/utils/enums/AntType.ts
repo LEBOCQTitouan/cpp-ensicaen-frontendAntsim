662c0060bb7cfa8d3b0e795ac5b5c6c3c3a89418
lebocqT <lebocq.titouan@gmail.com>
2023-01-02 18:33:26 +0100

    Minor edit to be on point with cpp model

--- src/server/common/utils/enums/AntType.ts
+++ src/server/common/utils/enums/AntType.ts
@@ -1,7 +1,6 @@
 export enum AntType {
-  LARVAE = 0, // care this does not fit the model defined in the API
-  WARRIOR = 1,
-  WORKER = 2,
-  QUEEN = 3,
-  SCOUT = 4
+  QUEEN,
+  WORKER,
+  SOLDIER,
+  SCOUT
 }
