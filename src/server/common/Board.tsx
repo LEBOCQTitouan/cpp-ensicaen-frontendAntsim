6dfa040927f5810851bfc89c78e34bcddd487a4d
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2023-01-03 23:14:20 +0100

    Basic functionnal zoom and translation

--- src/server/common/Board.tsx
+++ src/server/common/Board.tsx
@@ -1,17 +1,106 @@
-import React, { useEffect, useState } from "react";
-import MapCase from "./MapCase";
+import React, { useEffect, useRef, useState, WheelEvent, MouseEvent} from "react";
 import { AntMap } from "./utils/Map";
 import { Tile } from "./utils/Tile";
+import { UtilDrawing } from "./UtilCanvas";
+import { TileType } from "./utils/enums/TileType";
+
+/**
+ * util function used to get a metric used to display as mush element (of same size) as possible on a canvas
+ * @param canvas
+ * @param n the maximum number of elements on both horizontal and vertical axis
+ */
+const getCanvasMetric = (canvas : HTMLCanvasElement, n : number) : number => {
+  if (canvas.height > canvas.width) {
+    return canvas.width / n;
+  }
+  return canvas.height / n;
+}
+
+/**
+ * util function used to find the greatest size in all arrays in a matrix (lines and cols)
+ * @param matrix
+ */
+const getMaxArraySizeInMatrix = (matrix : any [][]) : number => {
+  let max_size : number = matrix.length;
+  for (const arrayElement of matrix) {
+    if (arrayElement.length > max_size) {
+      max_size = arrayElement.length;
+    }
+  }
+  return max_size
+}
+
+/**
+ * util function used to find the greatest size in sub arrays in a matrix
+ * @param matrix
+ */
+const getMaxSizeArrayInsideMatrix = (matrix : any[][]) : number => {
+  let max_size = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    const arrayCol : any[] | undefined = matrix[i];
+    if (arrayCol != undefined && max_size < arrayCol.length) {
+        max_size = arrayCol.length;
+    }
+  }
+  return max_size;
+}
+
+const drawTileBackground = (context : CanvasRenderingContext2D, tile : Tile, x : number, y : number, util : UtilDrawing) => {
+  switch (tile.type) {
+    case TileType.ROCK:
+      context.fillStyle = "#808080";
+    break;
+    case TileType.COLONY:
+      context.fillStyle = "#964B00";
+      break;
+    case TileType.DEFAULT:
+      context.fillStyle = "#00FF00"
+      break;
+  }
+  context.fillRect(x, y, util.metric * util.scaling, util.metric * util.scaling);
+}
+const drawTile = (canvas : HTMLCanvasElement, tile : Tile, x : number, y : number, util : UtilDrawing) : void => {
+  const context : CanvasRenderingContext2D | null = canvas.getContext('2d');
+  if (context == null)
+    return;
+  drawTileBackground(context, tile, x, y, util);
+}
+
+const drawAntMap = (canvas : HTMLCanvasElement, map : AntMap, util : UtilDrawing) : void => {
+  const context : CanvasRenderingContext2D | null = canvas.getContext('2d');
+  if (context == null)
+    return;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "#ffffff";
+
+  for (let i = 0; i < map.tiles.length; i++) {
+    const tileCol : Tile[] | undefined = map.tiles[i];
+    if (tileCol != undefined) {
+      for (let j = 0; j < tileCol.length; j++) {
+        const tile : Tile | undefined = tileCol[j];
+        if (tile != undefined) {
+          const x : number = i*util.metric*util.scaling + util.offsetX;
+          const y : number = j*util.metric*util.scaling + util.offsetY;
+          drawTile(canvas, tile, x, y, util);
+        }
+      }
+    }
+  }
+}
 
 const Board: React.FC = (props) => {
+  // TODO update metric on canvas update
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [json, setJson] = useState<AntMap>();
-  const [scaling, setScaling] = useState<number>(0);
 
-  const [mouseDown, setMouseDown] = useState<boolean>(false);
-  const [startDragX, setDragX] = useState<number>(0);
-  const [startDragY, setDragY] = useState<number>(0);
+  const [shiftDown, setShiftDown] = useState<boolean>(false);
+
   const [offsetX, setOffsetX] = useState<number>(0);
   const [offsetY, setOffsetY] = useState<number>(0);
+  const [scaling, setScaling] = useState<number>(1);
+  const [metric, setMetric] = useState<number>(0)
+
+  const refreshingRateApi = 10000;
 
   const fetchData = async () => {
     try {
@@ -19,7 +108,6 @@ const Board: React.FC = (props) => {
         if (!response.ok) {
           throw new Error(response.statusText);
         }
-        console.debug(response);
         return response.json();
       }).then(data => {
         setJson(data);
@@ -31,62 +119,53 @@ const Board: React.FC = (props) => {
 
   useEffect(() => {
     fetchData().then(r => {return;});
+    const interval = setInterval(() => {
+      fetchData().then(r => {return;});
+    }, refreshingRateApi);
+    return () => {
+      clearInterval(interval);
+    }
   }, []);
 
-  const handleZoom = (event: React.WheelEvent<HTMLDivElement>) => {
-    /*
-    // handling scroll
-    const delta = event.deltaY;
-    if (delta > 0) {
-      // User is scrolling down (zoom out)
-      setScaling(scaling - 0.1);
-    } else if (delta < 0) {
-      // User is scrolling up (zoom int)
-      setScaling(scaling - 0.1);
-    }
-    */
-  };
-
-  const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
-    setMouseDown(true);
-  };
-
-  const handleMouseUp = (event: React.MouseEvent<HTMLDivElement>) => {
-    setMouseDown(false);
-  };
-
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!mouseDown)
+  useEffect(() => {
+    const canvas : HTMLCanvasElement | null = canvasRef.current;
+    if (canvas == null || json == null)
       return;
-  };
+    if (metric == 0) {
+      const new_metric = getCanvasMetric(canvas, getMaxArraySizeInMatrix(json.tiles));
+      setMetric(new_metric);
+      setOffsetX(canvas.width/2 - new_metric * json.tiles.length / 2);
+      setOffsetY(canvas.height/2 - new_metric * getMaxSizeArrayInsideMatrix(json.tiles) / 2);
+    }
+    drawAntMap(canvas, json, {metric, offsetX, offsetY, scaling});
+  }, [json]);
+
+  const handleWheelEvent = (event : WheelEvent<HTMLCanvasElement>) => {
+    if (json == null)
+      return;
+    if (event.shiftKey) {
+      const event_value = (event.deltaX + event.deltaY) / 1000;
+      if (event_value + scaling != 0) {
+        setScaling(scaling + event_value);
+      }
+    } else {
+      const delta_x = event.deltaX / 10;
+      const delta_y = event.deltaY / 10;
+      setOffsetX(offsetX + delta_x);
+      setOffsetY(offsetY + delta_y);
+    }
+    const canvas : HTMLCanvasElement | null = canvasRef.current;
+    if (canvas == null || json == null)
+      return;
+    drawAntMap(canvas, json, {metric, offsetX, offsetY, scaling});
+  }
 
   return (
-    <div
-      onWheel={handleZoom}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onMouseMove={handleMouseMove}
-
-      className={"bg-black " +
-        "w-full h-full aspect-square " +
-        "inline-block " +
-        "overflow-hidden " +
-        "flex flex-col " +
-        "transform-gpu " +
-        "scale-" + scaling + " " +
-        "translate-x-" + Math.trunc(offsetX) + " " +
-        "translate-y-" + Math.trunc(offsetY)}
-    >
-      {json?.tiles.map((col, i) => {
-        return  (
-          <div key={"col"+i} className={"flex grow"}>{
-            col.map((t, j) => {
-              return <MapCase key={"case("+i+","+j+")"} t={t}></MapCase>
-            })
-          }</div>
-        )
-      })}
-    </div>
+    <canvas
+      ref={canvasRef}
+      onWheel={handleWheelEvent}
+      className={"h-full w-full bg-black"}
+    ></canvas>
   );
 }
 
---
+++ src/server/common/UtilCanvas.ts
@@ -0,0 +1,6 @@
+export interface UtilDrawing {
+  metric : number;
+  offsetX : number;
+  offsetY : number;
+  scaling : number;
+}
